e0ee9e7e449db7196c79717993df09fbe2dbb05a
Rodrigo <79727598+LorenStudio@users.noreply.github.com>
2021-02-28 05:26:44 -0300

    Update Config.cs
    Removed meaningless lines

--- Src/LorenLicenseSystem/LorenLicenseSystem/Config.cs
+++ Src/LorenLicenseSystem/LorenLicenseSystem/Config.cs
@@ -10,12 +10,7 @@ namespace LorenLicenseSystem
 {
     public static class Config
     {
-        public static string dbName, dbHost, dbUser, dbPass, hosIp, serverIp, udpVersion, ServerVersion;
-        // public static int dbPort;
-        public static ushort hosPort, maxDrop, syncPort;
-        public static bool sendFailMsg, useMaxAmmoInDrop, HostLogger;
-        public static float plantDuration, defuseDuration;
-        public static bool useHitMarker;
+        public static string dbName, dbHost, dbUser, dbPass, hosIp;
 
         public static void LoadDB()
         {
